ff9caab8ca83b1decd4972c224420bd83a235fe1
Dakota Tindle <daktin98500@gmail.com>
2025-08-17 14:50:31 -0500

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,10 @@
+import random
+rods=5
+condition=100
+crew=100
+mechanics=100
+status=0
+usage=100
+def status():
+    print(f"You have {rods} fuel rods left. The ship is at {condition}%.")
+    print(f"Your current fuel rod has {usage}% left")
